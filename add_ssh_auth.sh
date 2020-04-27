938eeff0fe10e76f8ffe202702d3dc2f56d6d343
ssarl <ssarl1180@gmail.com>
2020-04-27 17:41:43 +0300

    add windows OS, comments. It would work hopelly

--- add_ssh_auth.sh
+++ add_ssh_auth.sh
@@ -2,8 +2,14 @@
 
 #create ssh key
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.11
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.12
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.13
+# sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.12
+# sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.13
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.14
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.15
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.16
+# sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.15
+# sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.16
+
+# sshpass provides a variety of features to use ssh password
+# -p is password
+# -T is not to assign pseudo-tty when running
+# -o is option
+# StrictHostKeyChecking=no is to run without user's permit
---
+++ my_shell.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
